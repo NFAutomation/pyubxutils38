9be106b25a5a3c66e955dff800045c4b7ad2c88c
Dr. Ahrnbom <martin@nordicforestryautomation.com>
2026-02-17 16:11:54 +0100

    more stuff

--- src/pyubxutils/ubxtypes_core.py
+++ src/pyubxutils/ubxtypes_core.py
@@ -1,5 +1,6 @@
 # This is taken from a different repo, copy-pasted here for simplicity 
 # https://github.com/semuconsulting/pyubx2/blob/master/src/pyubx2/ubxtypes_core.py
+# https://github.com/semuconsulting/pyubx2/blob/master/src/pyubx2/exceptions.py
 
 
 """
@@ -539,3 +540,50 @@ UBX_MSGIDS = {
     # ***************************************************************
     b"\x66\x66": "FOO-BAR",
 }
+
+
+
+
+"""
+UBX Custom Exception Types.
+
+Created on 27 Sep 2020
+
+:author: semuadmin (Steve Smith)
+:copyright: semuadmin © 2020
+:license: BSD 3-Clause
+"""
+
+
+class ParameterError(Exception):
+    """Parameter Error Class."""
+
+
+class GNSSStreamError(Exception):
+    """Generic Stream Error Class."""
+
+
+class UBXParseError(Exception):
+    """
+    UBX Parsing error.
+    """
+
+
+class UBXStreamError(Exception):
+    """
+    UBX Streaming error.
+    """
+
+
+class UBXMessageError(Exception):
+    """
+    UBX Undefined message class/id.
+    Essentially a prompt to add missing payload types to UBX_PAYLOADS.
+    """
+
+
+class UBXTypeError(Exception):
+    """
+    UBX Undefined payload attribute type.
+    Essentially a prompt to fix incorrect payload definitions to UBX_PAYLOADS.
+    """
